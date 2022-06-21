902f252192804bea10076619146f8ef18294fedc
zhuchenyi <870357786@qq.com>
2022-06-21 10:48:28 +0800

    默认

--- app/src/main/java/cn/uni/lkb/SettingActivity.java
+++ app/src/main/java/cn/uni/lkb/SettingActivity.java
@@ -2,6 +2,7 @@ package cn.uni.lkb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
@@ -73,7 +74,45 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
+        String VideoEncoding = MMKVTools.getInstance().getString("VideoEncoding", "H264");
+        switch (VideoEncoding) {
+            case "H264":
+                //设置视频编码为H.264
+                binding.videoGp.check(binding.H264.getId());
+                break;
+            case "H263":
+                binding.videoGp.check(binding.H263.getId());
+                break;
+            case "MPEG_4":
+                binding.videoGp.check(binding.MPEG4.getId());
+                break;
+        }
 
+        String AudioEncoding=MMKVTools.getInstance().getString("AudioEncoding","AMR_NB");
+        switch (AudioEncoding) {
+            case "default":
+                //设置音频编码
+                binding.audioGp.check(binding.defaultAudio.getId());
+                break;
+            case "AMR_NB":
+                binding.audioGp.check(binding.AMRNB.getId());
+                break;
+            case "AAC":
+                binding.audioGp.check(binding.AAC.getId());
+                break;
+        }
 
+        String frameRate=MMKVTools.getInstance().getString("frameRate","rate_15");
+        switch (frameRate) {
+            case "rate_15":
+                binding.frameRateGp.check(binding.rate15.getId());
+                break;
+            case "rate_20":
+                binding.frameRateGp.check(binding.rate20.getId());
+                break;
+            case "rate_25":
+                binding.frameRateGp.check(binding.rate25.getId());
+                break;
+        }
     }
 }
